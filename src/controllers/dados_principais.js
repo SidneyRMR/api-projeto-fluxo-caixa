d88b56bad8470d060ab85084e612f856ea4ccbe6
Sidney Rafael Mantovani Ribeiro <sidneyrafael191@gmail.com>
2023-01-29 08:05:42 -0300

    retornado ao estado anterior

--- src/controllers/dados_principais.js
+++ src/controllers/dados_principais.js
@@ -1,70 +1,62 @@
-import {pool} from "../db";
+import {pool} from "../db"
 
 export const getDados = (_, res) => {
-    const q = "SELECT * FROM fluxocaixapessoal";
+    const q = "SELECT * FROM fluxocaixapessoal"
     pool.query(q, (err, data) => {
-        if (err) return res.status(500).json({ error: "Erro ao recuperar dados do banco de dados" });
-        return res.status(200).json(data);
-    });
-};
+        if (err) return res.json(err)
+        return res.status(200).json(data)
+    })
+}
+
 
 export const addDado = (req, res) => {
-    const data = req.body.data;
-    const descricao = req.body.descricao;
-    const valor = req.body.valor;
-    const parcela = req.body.parcela;
-    const entrada = req.body.entrada;
-    const quitado = req.body.quitado;
-    const observacao = req.body.observacao;
+    const q = "INSERT INTO fluxocaixapessoal(data, descricao, valor, parcela, entrada, quitado, observacao) VALUES ?"
 
-    if (!data || !descricao || !valor || typeof parcela === 'undefined' || typeof entrada === 'undefined' || typeof quitado === 'undefined') {
-        return res.status(400).json({ error: "Dados de entrada inválidos ou incompletos" });
-    }
-
-    const q = "INSERT INTO fluxocaixapessoal(data, descricao, valor, parcela, entrada, quitado, observacao) VALUES ?";
     const values = [[
-        data,
-        descricao,
-        valor,
-        parcela,
-        entrada, 
-        quitado, 
-        observacao || null, 
-    ]];
+        req.body.data,
+        req.body.descricao,
+        req.body.valor,
+        req.body.parcela,
+        req.body.entrada, 
+        req.body.quitado, 
+        req.body.observacao, 
+    ]]
+
     pool.query(q, [values], (err) => {
-        if(err) return res.status(500).json({ error: "Erro ao inserir dado no banco de dados" });
-        return res.status(200).json({ message: "Dado criado com sucesso!" });
-    });
-};
+        if(err) return res.json(err)
+        return res.status(200).json('Dado criado com sucesso!')
+    })
 
+}
 export const updateDado = (req, res) => {
-    const id = req.body.id;
-    const data = req.body.data;
-    const descricao = req.body.descricao;
-    const valor = req.body.valor;
-    const parcela = req.body.parcela;
-    const entrada = req.body.entrada;
-    const quitado = req.body.quitado;
-    const observacao = req.body.observacao;
+    const q = "UPDATE fluxocaixapessoal SET id = ?, data = ?, descricao = ?, valor = ?, parcela = ?, entrada = ?, quitado = ?, observacao = ? WHERE id = ?"
 
-    if (!id || !data || !descricao || !valor || typeof parcela === 'undefined' || typeof entrada === 'undefined' || typeof quitado === 'undefined') {
-        return res.status(400).json({ error: "Dados de entrada inválidos ou incompletos" });
-    }
+    const values = [
+        req.body.id,
+        req.body.data,
+        req.body.descricao,
+        req.body.valor,
+        req.body.parcela,
+        req.body.entrada,
+        req.body.quitado,
+        req.body.observacao,
+    ]
 
-    const q = "UPDATE fluxocaixapessoal SET data = ?, descricao = ?, valor = ?, parcela = ?, entrada = ?, quitado = ?, observacao = ? WHERE id = ?";
-    const values = [data, descricao, valor, parcela, entrada, quitado, observacao];
-    pool.query(q, [...values, id], (err) => {
-    if (err) return res.status(500).json({ error: "Erro ao atualizar dados no banco de dados" });
-    return res.status(200).json({ success: "Dados atualizados com sucesso" });
-    });
-    };
-    
-    export const deleteDado = (req, res) => {
-    const { id } = req.params;
-    if (!id) return res.status(400).json({ error: "ID inválido ou não informado" })
-    const q = "DELETE FROM fluxocaixapessoal WHERE `id` = ?";
-pool.query(q, [id], (err) => {
-    if (err) return res.status(500).json({ error: "Erro ao deletar dados no banco de dados" });
-    return res.status(200).json({ success: "Dados deletados com sucesso" });
-})
-    }
+    pool.query(q, [...values, req.params.id], (err) => {
+        if(err) return res.json(err)
+
+        return res.status(200).json('Dados atualizados com sucesso!')
+    })
+}
+
+// Deletar usuario funcionando!
+export const deleteDado = (req, res) => {
+    const q = "DELETE FROM fluxocaixapessoal WHERE `id` = ?"
+
+    pool.query(q, [req.params.id], (err) => {
+        if(err) return res.json(err)
+
+        return res.status(200).json('Dados deletados com sucesso!')
+    })
+
+}
